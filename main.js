deb79a5fe32af6553d1e17ef8b4cdde70ca4006a
feleshia33 <feleshiawest@gmail.com>
2022-04-07 14:42:20 -0500

    update portfolio

--- main.js
+++ main.js
@@ -1,39 +1,15 @@
-let id = (id) => document.getElementById(id);
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
 
-let classes = (classes) => document.getElementsByClassName(classes);
-
-let username = id("username"),
-    email = id("email"),
-    password = id("password"),
-    form = id("form"),
-    errorMsg = classes("error"),
-    successIcon = classes("success-icon"),
-    failureIcon = classes("failure-icon");
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    engine(username, 0, "User Name cannot be blank.");
-    engine(email, 1, "Email cannot be blank.");
-    engine(password, 2, "Password cannot be blank.");
-});
-
-let engine = (id, serial, message) => {
-
-    if (id.value.trim() === "") {
-        errorMsg[serial].innerHTML = message;
-        id.style.border = "2px solid red"
-
-        failureIcon[serial].style.opacity = "1";
-        successIcon[serial].style.opacity = "0";
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
     }
     else {
-        errorMsg[serial].innerHTML= "";
-        id.style.border = "2px solid green";
-
-        failureIcon[serial].style.opacity = "0";
-        successIcon[serial].style.opacity = "1";
+        header.classList.remove('navbarDark');
     }
 }
 
 
+   
